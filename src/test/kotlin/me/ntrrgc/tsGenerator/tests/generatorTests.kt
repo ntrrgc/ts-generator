75ebf49b7b0447a7103fa9754d9b1c949b2d4e1c
Alicia Boya García <ntrrgc@gmail.com>
2018-12-08 18:39:04 +0100

    Revert "Use correct nullability for mapped types."
    This reverts commit 65aee44e7ed85a4ae4e3c6556c9d4b6b47de4868.
    
    The fix was already upstream (my local repo was slightly outdated). This
    commit still added a test, but there others testing the same, so no need
    for it.

--- src/test/kotlin/me/ntrrgc/tsGenerator/tests/generatorTests.kt
+++ src/test/kotlin/me/ntrrgc/tsGenerator/tests/generatorTests.kt
@@ -294,14 +294,6 @@ interface ClassWithDependencies {
     """), mappings = mapOf(String::class to "CustomString"))
     }
 
-    it("supports nullable mapped types") {
-        assertGeneratedCode(ClassWithNullables::class, setOf("""
-interface ClassWithNullables {
-    widget: string | undefined;
-}
-"""), mappings = mapOf(Widget::class to "string"), voidType = VoidType.UNDEFINED)
-    }
-
     it("supports transforming property names") {
         assertGeneratedCode(DataClass::class, setOf("""
     interface DataClass {
